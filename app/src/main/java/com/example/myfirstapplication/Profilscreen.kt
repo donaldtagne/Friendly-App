9a551f38ea5c48e67d5c438e7e520c8e7f608259
donald <inf3604@hs-worms.de>
2022-01-30 17:27:05 +0100

    hallo

--- app/src/main/java/com/example/myfirstapplication/Profilscreen.kt
+++ app/src/main/java/com/example/myfirstapplication/Profilscreen.kt
@@ -1,11 +1,61 @@
 package com.example.myfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.activity_privacy.*
+import kotlinx.android.synthetic.main.activity_profilscreen.*
+import java.util.*
 
 class Profilscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profilscreen)
+
+        Goback.setOnClickListener {
+            val intent= Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+        }
+        tvprivacy.setOnClickListener {
+            val intent = Intent(this, Privacy::class.java)
+            startActivity(intent)
+        }
+        tvabmelden.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        tvhobby.setOnClickListener {
+            val intent= Intent(this,  hobbysanpassung::class.java)
+            startActivity(intent)
+        }
+        var selectedItemIndex = 0
+        tvkonto.setOnClickListener {
+
+            val onlinearray = arrayOf(
+                "ich habe gefunden, was ich suchte.",
+                "Die Leute sind unfreundlich.",
+                "Die App gefällt mir nicht.",
+                "Ich  bin nur neugierig.",
+                )
+            var selectedRingtone = onlinearray[selectedItemIndex]
+
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Warum löschen sie Ihr Account")
+                .setIcon(R.drawable.ic_action_name)
+                .setSingleChoiceItems(onlinearray, selectedItemIndex) { dialog, which ->
+                    selectedItemIndex = which
+                    selectedRingtone = onlinearray[which]
+                }
+                .setPositiveButton("Ok") { dialog, which ->
+                    //showSnackbar("$selectedRingtone Selected")
+                }
+                .setNegativeButton("Cancel") { dialog, which ->
+                    dialog.cancel()
+                }
+                .show()
+        }
     }
 }
+
--- app/src/main/java/com/example/myfirstapplication/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myfirstapplication/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstapplication.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,22 +8,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirstapplication.IdentityVerificationScreen
+import com.example.myfirstapplication.Profilscreen
 import com.example.myfirstapplication.R
+import com.example.myfirstapplication.databinding.ActivityMainBinding
 import com.google.android.material.internal.ToolbarUtils
 import com.google.android.material.slider.Slider
 import com.skyfishjy.library.RippleBackground
+import kotlinx.android.synthetic.main.activity_interesse.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: HomeFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+       return inflater.inflate(R.layout.fragment_home, container, false)
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,6 +43,17 @@ class HomeFragment : Fragment() {
        ivRipple.setOnClickListener {
            rippleBackground.startRippleAnimation()
        }
+        ibToolbarProfile.setOnClickListener {
+            requireActivity().run {
+                startActivity(Intent(this, Profilscreen::class.java))
+                finish()
+            }
+        }
+
+
+
+
 
     }
+
 }
